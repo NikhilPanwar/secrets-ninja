8de147cf40dd58e5f3122e2e939d11d7a0b5216f
NIKHIL PANWAR <40316546+NikhilPanwar@users.noreply.github.com>
2024-02-24 21:35:05 +0530

    added universal json

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,6 @@ import FlowbiteNavbar from './components/navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SB from './components/sidebar';
 import FT from './components/footer';
-import Dashboard from './components/dashboard';
-import Stripe from './modules/stripe';
-import Github from './modules/github';
-import Shopify from './modules/shopify';
-import Telegram from './modules/telegram';
-import OpenAI from './modules/openai';
-import Paystack from './modules/paystack';
-import Omnisend from './modules/omnisend';
-import LaunchDarkly from './modules/launchdarkly';
-import Clearbit from './modules/clearbit';
-import Paypal from './modules/paypal';
 import { Flowbite, theme } from 'flowbite-react';
 import UniversalComponent from './modules/universal';
 
@@ -26,9 +15,9 @@ export default function MyPage() {
             <SB className="dark:bg-slate-700" />
             <div className="flex-1 bg-gray-100 dark:bg-gray-700 overflow-auto">
               <Routes>
-                <Route path="/" element={<UniversalComponent serviceType="Stripe" />} />
-                <Route path="/stripe" element={<Stripe />} />
-                <Route path="/github" element={<Github />} />
+                <Route path="/" element={<UniversalComponent serviceType="Paypal" />} />
+                <Route path="/stripe" element={<UniversalComponent serviceType="Stripe" />} />
+                {/* <Route path="/github" element={<Github />} />
                 <Route path="/shopify" element={<Shopify />} />
                 <Route path="/telegram" element={<Telegram />} />
                 <Route path="/openai" element={<OpenAI />} />
@@ -36,7 +25,7 @@ export default function MyPage() {
                 <Route path="/omnisend" element={<Omnisend />} />
                 <Route path="/launchdarkly" element={<LaunchDarkly />} />
                 <Route path="/clearbit" element={<Clearbit />} />
-                <Route path="/paypal" element={<Paypal />} />
+                <Route path="/paypal" element={<Paypal />} /> */}
               </Routes>
             </div>
           </div>
--- src/modules/universal.jsx
+++ src/modules/universal.jsx
@@ -1,17 +1,23 @@
-import React, { useState } from "react";
-import { Button, Dropdown, Label, TextInput } from 'flowbite-react';
+import React, { useState, useEffect } from "react";
+import { Button, Dropdown, Label, TextInput, Alert } from 'flowbite-react';
 import OutputWindow from "../components/output_window";
 import RequestWindow from "../components/request_window";
-import servicesConfig from '../data/detectors copy.json'; // Import your JSON configuration
+import servicesConfig from '../data/detectors.json';
+import { HiInformationCircle } from 'react-icons/hi';
 
 export default function UniversalComponent({ serviceType }) {
-    const [selectedEndpoint, setSelectedEndpoint] = useState("Select Endpoint");
-    const [curlCommand, setCurlCommand] = useState("");
-    const [requestURL, setRequestURL] = useState("");
-    const [requestMethod, setRequestMethod] = useState("");
-
     const serviceConfig = servicesConfig[serviceType] || {};
     const endpoints = serviceConfig.endpoints || {};
+    const defaultInputFields = serviceConfig.input_fields || {};
+
+    const firstEndpointKey = Object.keys(endpoints)[0];
+    const firstEndpoint = endpoints[firstEndpointKey] || {};
+
+    const [selectedEndpoint, setSelectedEndpoint] = useState(firstEndpoint.label || "Select Endpoint");
+    const [curlCommand, setCurlCommand] = useState(firstEndpoint.curl || "");
+    const [requestURL, setRequestURL] = useState(firstEndpoint.request_url || "");
+    const [requestMethod, setRequestMethod] = useState(firstEndpoint.request_method || "");
+    const [inputFields, setInputFields] = useState(firstEndpoint.override_default_input_field ? firstEndpoint.input_fields : defaultInputFields);
 
     const handleDropdownChange = (endpointKey) => {
         const endpointConfig = endpoints[endpointKey];
@@ -19,20 +25,56 @@ export default function UniversalComponent({ serviceType }) {
         setCurlCommand(endpointConfig.curl);
         setRequestURL(endpointConfig.request_url);
         setRequestMethod(endpointConfig.request_method);
+
+        if (endpointConfig.override_default_input_field) {
+            setInputFields(endpointConfig.input_fields || {});
+        } else {
+            setInputFields(defaultInputFields);
+        }
     };
+
+    useEffect(() => {
+        // Resetting the state variables when serviceType changes
+        const newServiceConfig = servicesConfig[serviceType] || {};
+        const newEndpoints = newServiceConfig.endpoints || {};
+        const newDefaultInputFields = newServiceConfig.input_fields || {};
+
+        const firstNewEndpointKey = Object.keys(newEndpoints)[0];
+        const firstNewEndpoint = newEndpoints[firstNewEndpointKey] || {};
+
+        setSelectedEndpoint(firstNewEndpoint.label || "Select Endpoint");
+        setCurlCommand(firstNewEndpoint.curl || "");
+        setRequestURL(firstNewEndpoint.request_url || "");
+        setRequestMethod(firstNewEndpoint.request_method || "");
+        setInputFields(firstNewEndpoint.override_default_input_field ? firstNewEndpoint.input_fields : newDefaultInputFields);
+    }, [serviceType]); 
 
     return (
         <div className="p-10">
             <h2 className="text-2xl font-semibold dark:text-white">Check {serviceType} Keys</h2>
             <div className="py-10 flex w-full flex-col gap-4">
-                <div className="w-full">
-                    <div className="mb-2 block">
-                        <Label htmlFor="secret" value="Enter API Key" />
+                {Object.keys(inputFields).map((key) => (
+                    <div key={key} className="w-full">
+                        <div className="mb-2 block">
+                            <Label htmlFor={key} value={inputFields[key].label} />
+                        </div>
+                        <TextInput
+                            id={key}
+                            type={inputFields[key].type}
+                            placeholder={inputFields[key].placeholder}
+                            required
+                        />
                     </div>
-                    <TextInput id="secret" type="text" placeholder="sk_live_xxxx" required />
-                </div>
+                ))}
+
+                {serviceConfig.alert && (
+                    <Alert color={serviceConfig.alert.color} icon={HiInformationCircle}>
+                        <span className="font-medium">Alert!</span> {serviceConfig.alert.alert_message}
+                    </Alert>
+                )}
+
                 <div className="flex flex-col gap-2">
-                    <Label htmlFor="secret" value="Select Endpoint" />
+                    <Label value="Select Endpoint" />
                     <div className="flex gap-2">
                         <Dropdown label={selectedEndpoint}>
                             {Object.keys(endpoints).map((key) => (
