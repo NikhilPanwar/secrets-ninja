2ae05a610fa8b6141771653135a74d92c5a01f89
NIKHIL PANWAR <40316546+NikhilPanwar@users.noreply.github.com>
2024-02-27 21:52:32 +0530

    formatted table

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -1,55 +1,42 @@
-
 'use client';
 
 import { Table } from 'flowbite-react';
+import servicesConfig from '../data/detectors.json';
 
 function MainPageTable() {
+  // Helper function to capitalize first letter and replace underscores
+  const formatEndpointName = (name) => {
+    return name
+      .split('_')
+      .map(part => part.charAt(0).toUpperCase() + part.slice(1))
+      .join(' ');
+  };
+
+  // Function to transform service config into rows for rendering
+  const renderServiceRows = () => {
+    return Object.entries(servicesConfig).map(([serviceName, serviceDetails]) => (
+      <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800" key={serviceName}>
+        <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+          {serviceName}
+        </Table.Cell>
+        <Table.Cell>
+          {Object.keys(serviceDetails.endpoints)
+            .map(endpointName => formatEndpointName(endpointName))
+            .join(', ')}
+        </Table.Cell>
+      </Table.Row>
+    ));
+  };
+
   return (
     <div className="overflow-x-auto">
       <Table hoverable>
         <Table.Head>
-          <Table.HeadCell>Key Type</Table.HeadCell>
-          <Table.HeadCell>Regex</Table.HeadCell>
+          <Table.HeadCell>Service Name</Table.HeadCell>
           <Table.HeadCell>Endpoints</Table.HeadCell>
-          <Table.HeadCell>
-            <span className="sr-only">Edit</span>
-          </Table.HeadCell>
         </Table.Head>
         <Table.Body className="divide-y">
-          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              {'Stripe'}
-            </Table.Cell>
-            <Table.Cell>{'[rs]k_live_[a-zA-Z0-9]{20,247}'}</Table.Cell>
-            <Table.Cell>Balance, Customers, Orders, Payment Intents, Account</Table.Cell>
-            <Table.Cell>
-              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-                Submit Endpoint
-              </a>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              Github
-            </Table.Cell>
-            <Table.Cell>ghp_******</Table.Cell>
-            <Table.Cell>User, Repos</Table.Cell>
-            <Table.Cell>
-              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-              Submit Endpoint
-              </a>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">Shopify</Table.Cell>
-            <Table.Cell>(shppa_|shpat_)([0-9A-Fa-f]{32})</Table.Cell>
-            <Table.Cell>Customers, Orders</Table.Cell>
-            <Table.Cell>
-              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-              Submit Endpoint
-              </a>
-            </Table.Cell>
-          </Table.Row>
+          {renderServiceRows()}
         </Table.Body>
       </Table>
     </div>
