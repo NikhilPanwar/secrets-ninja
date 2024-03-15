b686182e8cca839bca80a7298a3d7493302d0918
NIKHIL PANWAR <40316546+NikhilPanwar@users.noreply.github.com>
2024-03-15 16:43:19 +0530

    created seperate copy button component

--- src/components/output_window.jsx
+++ src/components/output_window.jsx
@@ -1,37 +1,31 @@
 import { Card, Button } from 'flowbite-react';
-import { FaRegClipboard } from "react-icons/fa";
 import JSONPretty from "react-json-pretty";
 import 'react-json-pretty/themes/1337.css';
+import CopyButton from './copy_button';
 
 function OutputWindow({ status_code = 0, output_str = '{}' }) {
 
   if (status_code === 0) {
-    return null; // or return <></> for an empty fragment
+    return null;
   }
-
-  const copyToClipboard = () => {
-    navigator.clipboard.writeText(output_str).then(() => {
-      alert("Copied to clipboard!");
-    }).catch(err => {
-      console.error('Could not copy text: ', err);
-    });
-  };
 
   return (
     <Card className="w-full">
-      <div className="flex justify-between items-center flex-wrap">
-        <h2 className="text-lg font-semibold dark:text-white">Response</h2>
-        <Button.Group>
-          <Button color="gray" size="xs" onClick={copyToClipboard}><FaRegClipboard className="mr-2 h-3 w-3" />Copy</Button>
-          <Button color="gray" size="xs">{status_code === 200 ? '🟢' : '🔴'} {status_code}</Button>
-        </Button.Group>
+    <div className="flex justify-between items-center flex-wrap">
+      <h2 className="text-lg font-semibold dark:text-white">Response</h2>
+      <div style={{ display: 'flex', gap: '8px' }}> {/* Adjusted part */}
+        <CopyButton textToCopy={output_str} />
+        <Button color="gray" size="xs">
+          {status_code === 200 ? '🟢' : '🔴'} {status_code}
+        </Button>
       </div>
-      <div className="overflow-auto" style={{ maxHeight: '58vh' }}>
-        <pre className="text-xs md:text-sm font-mono dark:text-white whitespace-pre-wrap">
-          <JSONPretty id="json-pretty" data={output_str} />
-        </pre>
-      </div>
-    </Card>
+    </div>
+    <div className="overflow-auto" style={{ maxHeight: '58vh' }}>
+      <pre className="text-xs md:text-sm font-mono dark:text-white whitespace-pre-wrap">
+        <JSONPretty id="json-pretty" data={output_str} />
+      </pre>
+    </div>
+  </Card>
   );
 }
 
--- src/components/request_window.jsx
+++ src/components/request_window.jsx
@@ -1,30 +1,19 @@
-import { Card, Button } from 'flowbite-react';
-import { FaRegClipboard } from "react-icons/fa";
+import React, { useState } from 'react';
+import { Card } from 'flowbite-react';
+import CopyButton from './copy_button';
 import 'react-json-pretty/themes/1337.css';
-import { IoMdSend } from "react-icons/io";
 
-function RequestWindow(curl = 'curl https://api.stripe.com/v1/balance -u sk_live_xxxx:') {
-
-  const copyToClipboard = () => {
-    navigator.clipboard.writeText(curl.curl).then(() => {
-      alert("Copied to clipboard!");
-    }).catch(err => {
-      console.error('Could not copy text: ', err);
-    });
-  };
+function RequestWindow({ curl = 'curl https://api.stripe.com/v1/balance -u sk_live_xxxx:' }) {
 
   return (
     <Card className="w-full">
       <div className="flex justify-between items-center flex-wrap">
         <h2 className="text-lg font-semibold dark:text-white">Request</h2>
-        {/* <Button.Group> */}
-          <Button color="gray" size="xs" onClick={copyToClipboard}><FaRegClipboard className="mr-2 h-3 w-3" />Copy</Button>
-          {/* <Button color="gray" size="xs"><IoMdSend className="mr-2 h-3 w-3" />Test</Button>
-        </Button.Group> */}
+        <CopyButton textToCopy={curl} />
       </div>
       <div className="overflow-auto" style={{ maxHeight: '58vh' }}>
         <pre className="text-xs md:text-sm font-mono dark:text-white whitespace-pre-wrap">
-          {curl.curl}
+          {curl}
         </pre>
       </div>
     </Card>
