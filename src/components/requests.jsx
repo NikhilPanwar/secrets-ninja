fe0ae62657ed618f7953bc0b1dc119065962bb14
NIKHIL PANWAR <40316546+NikhilPanwar@users.noreply.github.com>
2024-03-20 13:32:12 +0530

    added autopilot

--- src/components/requests.jsx
+++ src/components/requests.jsx
@@ -311,6 +311,14 @@ async function makeUniversalRequest(serviceType, inputData, endpointURL, request
                 }
             });
             break;
+        case 'Autopilot':
+            response = await fetch(endpointURL, {
+                method: requestMethod,
+                headers: {
+                    'autopilotapikey': `${inputData.api_key}`,
+                }
+            });
+            break;
         default:
             return { status: 400, data: { message: 'Unsupported service type' } };
     }
