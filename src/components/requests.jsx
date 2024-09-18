51355e1707eb30c1ec511d3b1cd1a4227fba2972
NIKHIL PANWAR <40316546+NikhilPanwar@users.noreply.github.com>
2024-09-19 01:57:27 +0530

    added buildkite support

--- src/components/requests.jsx
+++ src/components/requests.jsx
@@ -400,6 +400,14 @@ async function makeUniversalRequest(serviceType, inputData, endpointURL, request
                 }
             });
             break;
+        case 'Buildkite':
+            response = await fetch(endpointURL, {
+                method: requestMethod,
+                headers: {
+                    'Authorization': `Bearer ${inputData.api_key}`,
+                }
+            });
+            break;
         default:
             return { status: 400, data: { message: 'Unsupported service type' } };
     }
