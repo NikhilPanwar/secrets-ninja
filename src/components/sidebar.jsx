dbf7f9d0d05df0abdad50078e2f699adcc1a38a0
NIKHIL PANWAR <40316546+NikhilPanwar@users.noreply.github.com>
2025-04-15 10:23:16 -0700

    upodated icon

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -49,7 +49,7 @@ import { FaHubspot } from "react-icons/fa";
 import { FaAws } from "react-icons/fa";
 import { SiMongodb } from "react-icons/si";
 import { SiRabbitmq } from "react-icons/si";
-import { DiPostgresql } from "react-icons/di";
+import { BiLogoPostgresql } from "react-icons/bi";
 
 function SB({ visible, servicesConfig }) {
   // Accept visible as a prop
@@ -104,7 +104,7 @@ function SB({ visible, servicesConfig }) {
     AWS: FaAws,
     MongoDB: SiMongodb,
     RabbitMQ: SiRabbitmq,
-    Postgres: DiPostgresql
+    Postgres: BiLogoPostgresql
   };
 
   return (
